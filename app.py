7ab4055f57086a34a202f31d5d5264aa0c49dd97
sanjoydeb0007 <sanjoydeb0007@gmail.com>
2021-07-18 02:41:31 +0600

    login and bd connection

---
+++ app.py
@@ -0,0 +1,48 @@
+# Store this code in 'app.py' file
+
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+  
+  
+app = Flask(__name__)  
+app.secret_key = 'your secret key'
+  
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'geeklogin'
+  
+mysql = MySQL(app)
+  
+@app.route('/', methods =['GET', 'POST'])
+def index():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (username, password, email),(%s, % s, % s)', (username, password, email ))
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('index.html', msg = msg)
+
+
+if __name__ == "__main__":
+    app.run(host ="localhost", port = int("5080"))
